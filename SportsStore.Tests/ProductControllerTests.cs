c059e9ddf24f52167d4b32a9229b5c6ba6a9a040
Anjey Brusensky <anjey.brusensky@gmail.com>
2019-08-14 11:23:19 +0200

    Added model data to Products(with page info) changed 'List' action to work with new model Added test to check pagination edited Can_Paginate to work with new model

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -6,11 +6,39 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using SportsStore.Controllers;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Tests
 {
     public class ProductControllerTests
     {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup( m => m.Products ).Returns( (new Product[]
+                {
+                    new Product { ProductID = 1, Name = "P1" },
+                    new Product { ProductID = 2, Name = "P2" },
+                    new Product { ProductID = 3, Name = "P3" },
+                    new Product { ProductID = 4, Name = "P4" },
+                    new Product { ProductID = 5, Name = "P5" }
+                }).AsQueryable<Product>() );
+
+            ProductController controller = new ProductController( mock.Object ) { PageSize = 3 };
+
+            //
+            ProductsListViewModel result = controller.View( 2 ).ViewData.Model as ProductsListViewModel;
+
+            //
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal( 2, pageInfo.CurrentPage );
+            Assert.Equal( 3, pageInfo.ItemsPerPage );
+            Assert.Equal( 5, pageInfo.TotalItems );
+            Assert.Equal( 2, pageInfo.TotalPages );
+        }
+
         [Fact]
         public void Can_Paginate()
         {
@@ -29,10 +57,10 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             //Act
-            IEnumerable<Product> result = controller.List(2).ViewData.Model as IEnumerable<Product>;
+            ProductsListViewModel result = controller.List(2).ViewData.Model as ProductsListViewModel;
 
             //Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[0].Name);
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,6 +20,15 @@ namespace SportsStore.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1) => View(repository.Products.OrderBy(p => p.ProductID).Skip((productPage - 1) * PageSize).Take(PageSize));
+        public ViewResult List( int productPage = 1 ) => View( new ProductsListViewModel
+        {
+            Products = repository.Products.OrderBy( p => p.ProductID ).Skip( (productPage - 1) * PageSize ).Take( PageSize ),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Products.Count()
+            }
+        } );
     }
 }
