8f1cd02c287dbc18ac86d68b68991c74dbd44279
Anjey Brusensky <anjey.brusensky@gmail.com>
2019-08-26 15:27:10 +0200

    Added highlight to Selected Category

--- SportsStore.Tests/NavigationMebuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMebuViewComponentTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -31,6 +33,34 @@ namespace SportsStore.Tests
 
             //
             Assert.True( Enumerable.SequenceEqual( new string[] { "Apples", "Oranges", "Plums" }, results ));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            //
+            string categoryToSelect = "Apples";
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup( m => m.Products ).Returns( (new Product[] {
+                new Product {ProductID = 1, Name = "Pl", Category = "Apples" },
+                new Product {ProductID = 4, Name = "Р2", Category = "Oranges" },
+                }).AsQueryable<Product>() );
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent( mock.Object );
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new RouteData()
+                }
+            };
+
+            target.RouteData.Values["category"] = categoryToSelect;
+
+            //
+            string result = (string)(target.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+            //
+            Assert.Equal( categoryToSelect, result );
         }
     }
 }
